d875c6bd5ba8c309ac04dfe3d195c0398aae3849
Adam Henry <a.david.henry@gmail.com>
2008-09-10 14:09:22 -0700

    Polished inventory interface

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
 		number.to_s.gsub(/(\d)(?=\d{3}+(\.\d*)?\b)/, "\\1#{delimiter}")
 	end
 
+	def list_item_right_left name, varible
+		render( :partial => "list_item",
+					:locals => {
+						:locals => varible,
+						:left => name.to_s +  "_left",
+						:right => name.to_s + "_right"
+					}
+				)
+	end
+
 end
